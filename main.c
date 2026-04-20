ade5762b908714fb62c4ad03359ac7cfa57c21c8
Teodora-Maria Crai <teodora-maria.crai@student.upt.ro>
2026-04-20 21:27:34 +0300

    added all operations besides filter

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <fcntl.h>
 typedef struct {
     float lat,lng; //latitude and longitude
@@ -93,14 +95,14 @@ void add_report(char *district_id,char *role,char *user) {
     //check for necessary permissions
     if (check_permission(path, role, 'w')==0) {
         printf("Error: %s %s does not have permission to write in file.",role,user);
-        exit(-1);
+        return;
     }
     //write the data to the newly created file
     int fd = open(path, O_WRONLY | O_CREAT | O_APPEND, 0664); //open the file, pipeline a series of flags:
     //open to writitng only, create if not created, otherwise no effect and file offset shall be set to the end of the file prior to each write
     if (fd == -1) {
         printf("Failed to open reports file");
-        exit(-1);
+        return;
     }
     if (write(fd, &new_report, sizeof(Report)) == -1) {
         printf("Failed to write report");
@@ -140,13 +142,13 @@ void list(const char *district_id,const char *role) {
     //check for necessary permissions
     if (check_permission(path, role, 'r')==0) {
         printf("Error: %s does not have permission to read reports file in %s.",role,district_id);
-        exit(-1);
+        return;
     }
     //get file stats
     struct stat st;
     if (stat(path, &st) == -1) {
         printf("Error accessing reports.dat in directory: %s",district_id);
-        exit(-1);
+        return;
     }
     //get permissions in the specified format
     char permissions[10];
@@ -163,7 +165,7 @@ void list(const char *district_id,const char *role) {
     int fd = open(path, O_RDONLY);
     if (fd == -1) {
         printf("Could not open file for reading");
-        exit(-1);
+        return;
     }
     Report rep;
     while (read(fd,&rep,sizeof(Report))==sizeof(Report)) {
@@ -178,13 +180,13 @@ void view(char *district_id,int report_id,char *role) {
     //check for necessary permissions
     if (check_permission(path, role, 'r')==0) {
         printf("Error: %s does not have permission to read reports file in %s.",role,district_id);
-        exit(-1);
+        return;
     }
     //open reports file
     int fd = open(path, O_RDONLY);
     if (fd == -1) {
         printf("Could not open file for reading");
-        exit(-1);
+        return;
     }
     Report rep;
     int ok=0; //binary variable to check if report was found
@@ -204,6 +206,74 @@ void view(char *district_id,int report_id,char *role) {
         }
     }
     if (ok==0) printf("Could not find report %d.",report_id);
+    close(fd);
+}
+void remove_report(const char *district_id,int report_id,const char *role) {
+    if (strcmp(role,"manager")!=0) {
+        printf("Only managers can perform this operation!");
+        return;
+    }
+    char path[512];
+    snprintf(path, sizeof(path), "%s/reports.dat", district_id);
+    //check for necessary permissions
+    if (check_permission(path, role, 'r')==0 || check_permission(path, role, 'w')==0) {
+        printf("Error: %s does not have permission to read/write in reports file in %s.",role,district_id);
+        return;
+    }
+    //open reports file
+    int fd = open(path, O_RDWR);
+    if (fd == -1) {
+        printf("Could not open file for reading/writing");
+        return;
+    }
+    Report rep;
+    long pos=-1;
+    while (read(fd,&rep,sizeof(Report))==sizeof(Report)) {
+        if (rep.id == report_id) {
+            report_count--;
+            pos=lseek(fd,0,SEEK_CUR)-sizeof(Report); //starting position of the report we must delete
+            break;
+        }
+    }
+    if (pos==-1) return;
+    long current_pos=pos;
+    while (1) {
+        lseek(fd,current_pos+sizeof(Report),SEEK_SET); //shift forward one record
+        if (read(fd,&rep,sizeof(Report))!=sizeof(Report)) {
+            break; //if we can't read the next report in line, it means we reached eof
+            //and can end the back and forth shift process
+        }
+        lseek(fd,current_pos,SEEK_SET);//shift back to current record
+        write(fd,&rep,sizeof(Report));//overwrite it with the data that was just read
+        current_pos+=sizeof(Report); //advance through the reports
+    }
+    if (pos!=-1) ftruncate(fd,report_count*sizeof(Report)); //readjusts file size to one less report
+    close(fd);
+}
+void update_treshold(char *district_id,int value,char *role) {
+    if (strcmp(role,"manager")!=0) {
+        printf("Only managers can perform this operation!");
+        return;
+    }
+    char path[512];
+    snprintf(path, sizeof(path), "%s/district.cfg", district_id);
+    //check for necessary permissions
+    if (check_permission(path, role, 'w')==0) {
+        printf("Error: %s does not have permission to write in reports file in %s.",role,district_id);
+        return;
+    }
+    //q: trebuie sa mai verific o data explicit 640? ce inseamna diagnostic?
+    //open configuration file
+    int fd = open(path, O_WRONLY | O_TRUNC); //O_TRUNC empties file before writing in it
+    if (fd == -1) {
+        printf("Could not open file for writing");
+        return;
+    }
+    char val_to_string[10];
+    int len=snprintf(val_to_string, sizeof(val_to_string), "%d", value); //turn integer into string as district.cfg is a text file
+    if (write(fd,val_to_string,len)!=len) {
+        printf("Error writing to file");
+    }
     close(fd);
 }
 int main(int argc, char * argv[]) {
